8ddc1c48996086712dddd9ebd24ea84fb3d65796
Emanuele Nencioni <emanencio@hotmail.com>
2025-05-03 21:50:27 +0200

    Added simple training loop

--- model/ssl.py
+++ model/ssl.py
@@ -20,3 +20,23 @@ def clip_contrastive_loss(z1, z2, temperature=0.07):
     loss_i2t = F.cross_entropy(logits, labels)
     loss_t2i = F.cross_entropy(logits.t(), labels)
     return (loss_i2t + loss_t2i) / 2
+
+def train_contrastive(model1, model2, dataloader, optimizer, device, temperature=0.07):
+    model1.train()
+    model2.train()
+    total_loss = 0.0
+    for x1, x2 in dataloader:
+        x1, x2 = x1.to(device), x2.to(device)
+
+        optimizer.zero_grad()
+
+        z1 = model1(x1)
+        z2 = model2(x2)
+
+        loss = clip_contrastive_loss(z1, z2, temperature)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(dataloader)
