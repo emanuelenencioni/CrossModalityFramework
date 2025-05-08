0ee2e01b004d8818737067fdff963b8a4f50e842
Emanuele Nencioni <emanencio@hotmail.com>
2025-05-08 21:45:12 +0200

    Added loss file

---
+++ training/loss.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class CLIP_loss(nn.Module):
+    """
+        CLIP-style symmetric cross-entropy loss between two embeddings.
+        z1, z2: [batch_size, dim]
+    """
+    def __init__():
+        super(self)
+        #learnable temerature
+        self.temp = nn.Parameter(torch.ones([]) * torch.log(torch.tensor(1 / 0.07)))
+
+    def forward(z1, z2, temperature=0.07):
+        batch_size = z1.size(0)
+
+        z1_emb = F.normalize(z1, p=2, dim=-1)
+        z2_emb = F.normalize(z2, p=2, dim=-1)
+        # Cosine similarity logits
+        logits = self.temp.exp() * torch.matmul(image_embeddings, text_embeddings.T)
+
+        labels = torch.arange(batch_size, device=z1.device)
+
+        loss_i2t = F.cross_entropy(logits, labels)
+        loss_t2i = F.cross_entropy(logits.t(), labels)
+        return (loss_i2t + loss_t2i) / 2
