dafab6207e44d76f35a7214f67f25e7eafd2091a
Emanuele Nencioni <emanencio@hotmail.com>
2025-05-08 22:27:35 +0200

    again fixing path..

--- dataset/create_dataset_txt.py
+++ dataset/create_dataset_txt.py
@@ -141,11 +141,11 @@ if __name__ == '__main__':
 
     images_to_events_index_all(opt.root_dir)
     create_dsec_dataset(dst_path=opt.root_dir,
-                        dataset_txt_path='./night_dataset.txt',
+                        dataset_txt_path=dir_path + '/night_dataset.txt',
                         events_num=0, labels_txt=False, labels_range=labels_range, image_change_num=1,
                         warp_images_flag=False)
 
     create_dsec_dataset(dst_path=opt.root_dir,
-                        dataset_txt_path='./night_test_dataset.txt',
+                        dataset_txt_path=dir_path + '/night_test_dataset.txt',
                         events_num=0, labels_txt=True, labels_range=None, image_change_num=1,
                         warp_images_flag=False)
