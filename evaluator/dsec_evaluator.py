525e79116ff8ff16d16225f63bd579ee5ef16de9
Emanuele <emanuele.nencioni1@gmail.com>
2025-09-19 22:03:45 +0200

    cleaning code

--- evaluator/dsec_evaluator.py
+++ evaluator/dsec_evaluator.py
@@ -305,14 +305,6 @@ class DSECEvaluator:
             output_batch, images_info[0]
         ):
             if output is None:
-                # pred_data = {
-                #     "image_id": int(img_info.data['idx']),  # Need image ID for COCO format
-                #     "category_id": -1,
-                #     "bbox": [],
-                #     "score": 0.0,
-                #     "segmentation": [],
-                # }  # COCO json format
-                # data_list.append(pred_data)
                 continue
 
             output = output.cpu()
