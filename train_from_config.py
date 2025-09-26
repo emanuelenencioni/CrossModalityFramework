beccb291c699a36b46eecc984d66f68120d13dd4
Emanuele <emanuele.nencioni1@gmail.com>
2025-09-26 23:36:15 +0200

    defining init_wandb

--- train_from_config.py
+++ train_from_config.py
@@ -29,6 +29,23 @@ from utils.helpers import DEBUG, set_seed
 from utils import argparser as argp
 
 
+def init_wandb(cfg):
+    wandb_log = False
+    aug = "aug" if cfg['dataset'].get('use_augmentations', False) else  "noaug"
+    schedl_name = cfg['scheduler']['name'] if 'name' in cfg['scheduler'].keys() else ""
+    run_name = cfg['model']['backbone']['name'] if ('name' in cfg['model']['backbone'].keys() and cfg['model']['backbone']['name'] != '') else model.get_name()
+    run_name = f"{run_name}_{cfg['optimizer']['name']}_{schedl_name}_{aug}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    if 'logger' in cfg.keys() and 'name' in cfg['logger'].keys():
+        if(cfg['logger']['name'] == 'wandb'):
+            wandb_cfg = cfg['logger']
+            assert 'project' in wandb_cfg.keys(), "specify 'project' wandb param"
+            assert 'entity' in wandb_cfg.keys(), "specify 'entity' wandb param"
+            wandb.init(project=wandb_cfg['project'], entity=wandb_cfg['entity'], name=run_name, config=cfg,settings=wandb.Settings(init_timeout=600))
+
+            wandb_log = True
+    return wandb_log
+
+
 if __name__ == "__main__":
     # Config loading && argument parser #
     cfg, pretrained_checkpoint = argp.parse_arguments()
@@ -45,31 +62,16 @@ if __name__ == "__main__":
     # Dataloader (CMDA)
     train_dl, test_dl = dl_builder.build_from_config(train_ds, test_ds, cfg)
 
-    wandb_log = False
-    aug = "aug" if cfg['dataset'].get('use_augmentations', False) else  "noaug"
-    schedl_name = cfg['scheduler']['name'] if 'name' in cfg['scheduler'].keys() else ""
-    run_name = cfg['model']['backbone']['name'] if ('name' in cfg['model']['backbone'].keys() and cfg['model']['backbone']['name'] != '') else model.get_name()
-    run_name = f"{run_name}_{cfg['optimizer']['name']}_{schedl_name}_{aug}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    if 'logger' in cfg.keys() and 'name' in cfg['logger'].keys():
-        if(cfg['logger']['name'] == 'wandb'):
-            wandb_cfg = cfg['logger']
-            assert 'project' in wandb_cfg.keys(), "specify 'project' wandb param"
-            assert 'entity' in wandb_cfg.keys(), "specify 'entity' wandb param"
-            wandb.init(project=wandb_cfg['project'], entity=wandb_cfg['entity'], name=run_name, config=cfg,settings=wandb.Settings(init_timeout=600))
-            # Log model architecture
-            wandb.watch(model)
-            wandb_log = True
+    wandb_log = init_wandb(cfg)
 
     device = cfg['device'] if 'device' in cfg.keys() else "cuda"
     model.to(device)    
 
-    # Place this right after model.to(device)
     if pretrained_checkpoint is not None:
         model.load_state_dict(pretrained_checkpoint['model_state_dict'])
         print("Pre-trained model loaded successfully")
 
     # Scheduler
-    schdlr = None
     if 'scheduler' in cfg.keys() and 'name' in cfg['scheduler'].keys() and cfg['scheduler']['name'] is not None:
         schdlr = scheduler_builder(opti, cfg['scheduler'])
         assert schdlr is not None, "Error - scheduler not correctly defined"
