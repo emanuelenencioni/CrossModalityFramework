e82806a9d2a383b518e751f4479d40118ed0e07c
Emanuele <emanuele.nencioni1@gmail.com>
2025-10-06 22:30:21 +0200

    adding domain in the run name

--- train_from_config.py
+++ train_from_config.py
@@ -34,7 +34,12 @@ def init_wandb(cfg):
     aug = "aug" if cfg['dataset'].get('use_augmentations', False) else  "noaug"
     schedl_name = cfg['scheduler']['name'] if 'name' in cfg['scheduler'].keys() else ""
     run_name = cfg['model']['backbone']['name'] if ('name' in cfg['model']['backbone'].keys() and cfg['model']['backbone']['name'] != '') else model.get_name()
-    run_name = f"{run_name}_{cfg['optimizer']['name']}_{schedl_name}_{aug}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    if cfg.get('dual_modality', True):
+        type_ = "dual" if cfg['dual_modality'] else "uni"
+    else:
+        type_ += "_event" if 'events' in cfg['model']['outputs'] else "_rgb"
+    run_name = f"{run_name}_{type_}_{cfg['optimizer']['name']}_{schedl_name}_{aug}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    
     if 'logger' in cfg.keys() and 'name' in cfg['logger'].keys():
         if(cfg['logger']['name'] == 'wandb'):
             wandb_cfg = cfg['logger']
@@ -43,6 +48,8 @@ def init_wandb(cfg):
             wandb.init(project=wandb_cfg['project'], entity=wandb_cfg['entity'], name=run_name, config=cfg,settings=wandb.Settings(init_timeout=600))
 
             wandb_log = True
+
+    cfg['run_name'] = run_name
     return wandb_log
 
 
