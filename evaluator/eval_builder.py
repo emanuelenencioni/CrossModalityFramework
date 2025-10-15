8783e475ace93fe2c3198ce0f35f8e9668cf57d1
Emanuele <emanencio@hotmail.com>
2025-10-15 23:57:07 +0200

    fixing building evaluator

--- evaluator/eval_builder.py
+++ evaluator/eval_builder.py
@@ -30,17 +30,17 @@ def build_from_config(data_loader, cfg: Dict[str, Any]):
     if cfg.get('dual_modality', True):
         in_size1 = cfg['model1']['input_size'] if 'input_size' in cfg['model1'].keys() else 512
         in_size2 = cfg['model2']['input_size'] if 'input_size' in cfg['model2'].keys() else 512
-        eval1 = _build_unimodal_dataset(dataset_type, data_loader[0], img_size=(in_size1, in_size1), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
-        eval2 = _build_unimodal_dataset(dataset_type, data_loader[1], img_size=(in_size2, in_size2), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
+        eval1 = _build_unimodal_evaluator(dataset_type, data_loader, img_size=(in_size1, in_size1), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
+        eval2 = _build_unimodal_evaluator(dataset_type, data_loader, img_size=(in_size2, in_size2), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
         return eval1, eval2
     else:
         in_size = cfg['model']['input_size'] if 'input_size' in cfg['model'].keys() else 512
-        return _build_unimodal_dataset(dataset_type, data_loader, img_size=(in_size, in_size), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
-        
+        return _build_unimodal_evaluator(dataset_type, data_loader, img_size=(in_size, in_size), confthre=confthre, nmsthre=nmsthre, num_classes=cfg['dataset']['bb_num_classes'], device=cfg['device'])
 
 
 
-def _build_unimodal_dataset(dataset_type, data_loader, img_size, confthre, nmsthre, num_classes, device):
+
+def _build_unimodal_evaluator(dataset_type, data_loader, img_size, confthre, nmsthre, num_classes, device):
     if dataset_type == 'dsec_night':
         return DSECEvaluator(data_loader, img_size=img_size, confthre=confthre, nmsthre=nmsthre, num_classes=num_classes, device=device)
     elif dataset_type == 'cityscapes':
